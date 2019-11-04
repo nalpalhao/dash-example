bd31728c94e968e6148c8c93647a4122d798688c
nalpalhao <57359525+nalpalhao@users.noreply.github.com>
2019-11-04 15:13:55 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 # Import required libraries
 import os
-from random import randint
-
 import plotly.plotly as py
 from plotly.graph_objs import *
 
@@ -16,13 +14,12 @@ import dash_html_components as html
 # Make sure not to change this file name or the variable names below,
 # the template is configured to execute 'server' on 'app.py'
 server = flask.Flask(__name__)
-server.secret_key = os.environ.get('secret_key', str(randint(0, 1000000)))
 
 # Put your Dash code here
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets, server=server)
+app = dash.Dash(__name__, server=server, external_stylesheets=external_stylesheets)
 
 app.layout = html.Div([
     html.H2('Hello World'),
@@ -42,4 +39,4 @@ def display_value(value):
 
 # Run the Dash app
 if __name__ == '__main__':
-    app.server.run(debug=True, threaded=True)
+    app.server.run(debug=True)
