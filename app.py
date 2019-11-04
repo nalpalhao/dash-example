40de506b38bcc2351cd3d81b80dffbbe42af9487
nalpalhao <57359525+nalpalhao@users.noreply.github.com>
2019-11-04 15:32:26 +0000

    Update app.py

--- app.py
+++ app.py
@@ -3,27 +3,23 @@ import os
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import pandas as pd
+import plotly.graph_objs as go
+import plotly.express as px
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+df_1 = pd.read_csv('plotly\\dv_data.csv')
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+fig = px.choropleth(df_1, locations='iso-a3', color='total', projection='orthographic', animation_frame='Year')
 
 server = app.server
 
 app.layout = html.Div([
-    html.H2('Hello World'),
-    dcc.Dropdown(
-        id='dropdown',
-        options=[{'label': i, 'value': i} for i in ['LA', 'NYC', 'MTL']],
-        value='LA'
-    ),
-    html.Div(id='display-value')
+    dcc.Graph(
+        figure=fig
+    )
 ])
-
-@app.callback(dash.dependencies.Output('display-value', 'children'),
-              [dash.dependencies.Input('dropdown', 'value')])
-def display_value(value):
-    return 'You have selected "{}"'.format(value)
 
 if __name__ == '__main__':
     app.run_server(debug=True)
