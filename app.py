7b1235d3cc178bc122415ef52a6bd24e4900c13d
nalpalhao <57359525+nalpalhao@users.noreply.github.com>
2019-11-04 15:02:34 +0000

    Update app.py

--- app.py
+++ app.py
@@ -24,8 +24,6 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets, server=server)
 
-server = app.server
-
 app.layout = html.Div([
     html.H2('Hello World'),
     dcc.Dropdown(
